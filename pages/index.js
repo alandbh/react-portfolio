b41cc9830d9ce8c2268e03ae809733b3781ad651
Alan Vasconcelos <alandbh@gmail.com>
2022-08-04 01:26:11 -0300

    Bio section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Heading from "../components/Heading";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -15,14 +16,14 @@ export default function Home() {
             </Head>
 
             <header className="md:grid grid-cols-14 gap-5 my-5">
-                <h1 className="col-start-2 col-span-5 tracking-[.15rem]">
+                <h1 className="col-start-2 col-span-5 tracking-[.15rem] text-white/60">
                     <b>ALAN</b> VASCONCELOS
                 </h1>
             </header>
 
-            <main className=" mt-[100px]">
+            <main className="mt-[100px]">
                 <div className="md:grid grid-cols-14 gap-5">
-                    <h2 className="hero col-start-2 col-span-8 text-7xl">
+                    <h2 className="hero col-start-2 col-span-8 text-7xl text-white/80">
                         Believe in the{" "}
                         <b>
                             <u>cleverness</u>
@@ -30,11 +31,51 @@ export default function Home() {
                         behind the interface.
                     </h2>
                 </div>
-                <div className="bio md:grid md:grid-cols-14 gap-5">
-                    <div className="col-span-2 col-start-2">01</div>
-                    <div className="col-span-6">02</div>
-                    <div className="col-span-4">03</div>
-                </div>
+                <section className="bio md:grid md:grid-cols-14 gap-5 mt-20">
+                    <div className="col-span-2 col-start-2 mt-12">
+                        <Image
+                            src="/photo.jpg"
+                            alt="Vercel Alan's photo"
+                            width={200}
+                            height={200}
+                        />
+                    </div>
+                    <div className="col-span-6 text-white/80">
+                        <Heading as="h3" className="text-white/50 mb-5 text-xs">
+                            About
+                        </Heading>
+                        <p>{`Hi! I'm Alan.
+I believe we need to research, prototype, test and refine the design for any interactive product. After all, just ”working” is not enough without a good experience.`}</p>
+
+                        <p>{`I'm graduated in Graphic Design with a master’s degree in Information Science. For more than 10 years I've been working as a usability and accessibility consultant for government agencies, as well for large companies. I'm also a co-author of the Brazilian Usability Booklet for e-Government agencies.`}</p>
+                    </div>
+                    <div className="col-span-4 text-white/80">
+                        <Heading as="h3" className="text-white/50 mb-5 text-xs">
+                            Client Experience
+                        </Heading>
+                        <p>{`Google Cloud, Banco do Brasil, Coca-Cola, Fundação Bradesco, Beam Suntory, PwC`}</p>
+
+                        <Heading
+                            as="h3"
+                            className="text-white/50 mb-5 mt-10 text-xs"
+                        >
+                            Client Experience
+                        </Heading>
+                        <p>{`Google Cloud, Banco do Brasil, Coca-Cola, Fundação Bradesco, Beam Suntory, PwC`}</p>
+                    </div>
+                </section>
+
+                <section className="mt-[120px] md:grid md:grid-cols-14 gap-5">
+                    <div className="flex justify-between items-center col-start-2 col-span-12 works-feed border-dotted border-t-2 border-white/50 pt-5">
+                        <Heading
+                            as="h1"
+                            className="text-4xl font-light text-white/50"
+                        >
+                            Works
+                        </Heading>
+                        <div>dsdsds</div>
+                    </div>
+                </section>
                 <h1 className="text-7xl">
                     Welcome to <a href="https://nextjs.org">Next.js!</a>
                 </h1>
