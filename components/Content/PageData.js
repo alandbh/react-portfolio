e53f6781a325e4dd00556f5c7aa9a5b1771e0481
Alan Vasconcelos <alandbh@gmail.com>
2022-08-05 17:38:56 -0300

    Content Object

---
+++ components/Content/PageData.js
@@ -0,0 +1,5 @@
+import { google_cloud } from "./google-cloud";
+
+export const PageData = {
+    "google-cloud": google_cloud,
+};
---
+++ components/Content/google-cloud.js
@@ -0,0 +1,51 @@
+export const google_cloud = {
+    title: "Google Cloud Heuristc Evaluation",
+    year: "2020 / 2021",
+    client: "Google Cloud",
+    role: "Analyst, Researcher",
+    project: [
+        `Looking at the major 25 retailers in Brazil, we aimed to find out how the overall user experience fared during the Black Friday period, comprising from the 20th to November 30th - both from a tech and user experience perspective.`,
+        `Using these learnings, we were then able to understand how brands could improve the overall experience and also, how Google Cloud could be a partner in helping make those improvements happen.`,
+        `The research was done during November 20th to 30th, 2021`,
+    ],
+    access: "https://bit.ly/3cVWmGI",
+    featured_image: "port-featured-google-cloud.png",
+    sections: [
+        {
+            title: "My role",
+            text: [
+                `Heuristic UX Evaluation for E-commerce. We defined 38 heuristics for e-commerce based on a full customer journey, divided into 8 categories.`,
+                `Each category is based on a stage of a purchase journey, starting from the search and identification of the product to the need for support.`,
+            ],
+            image: "port-sec1-google-cloud.svg",
+            color_class: "bg-gray-200",
+        },
+        {
+            title: "Actors",
+            text: [
+                `Two different user profiles who will try to purchase the product on the marketplace websites.`,
+            ],
+            image: "port-sec2-google-cloud.svg",
+        },
+        {
+            title: "ttt",
+            text: [`aa`, `aa`],
+            image: "port-sec3-google-cloud.png",
+        },
+        {
+            title: "ttt",
+            text: [`aa`, `aa`],
+            image: "port-sec4-google-cloud.png",
+        },
+        {
+            title: "ttt",
+            text: [`aa`, `aa`],
+            image: "port-sec5-google-cloud.png",
+        },
+        {
+            title: "ttt",
+            text: [`aa`, `aa`],
+            image: "port-t-fundacaobradesco.jpg",
+        },
+    ],
+};
