84b1e347f649ac4ec6a9e944a437041b1a8e937e
Alan Vasconcelos <alandbh@gmail.com>
2022-08-05 11:54:48 -0300

    Improving Card animation

--- components/Card/index.jsx
+++ components/Card/index.jsx
@@ -1,17 +1,40 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import Heading from "../Heading";
 import styles from "./Card.module.css";
 
 function Card({tags, title, href, image, className, filter}) {
 
-    let classFilter = 'hide';
+    const [hidden, setHidden] = useState(true)
 
-    if(String(tags).toLowerCase().includes(filter) || filter === 'all') {
-        classFilter = 'show'
-    }
+    useEffect(()=>{
+        if(!(String(tags).toLowerCase().includes(filter) || filter === 'all')) {
+            setHidden(true);
+        } else {
+            console.log(filter);
+            setHidden(false);
+            // setTimeout(()=>{
+            // }, 1000)
+        }
+    }, [filter, tags])
+
+    
+    // let classFilter = 'hide';
+    // useEffect(()=>{
+    //     console.log('trocou');
+    //     if(String(tags).toLowerCase().includes(filter) || filter === 'all') {
+    //         classFilter = 'show'    
+    //     }
+    //     setTimeout(()=>{
+
+    //     }, 1000)
+
+
+    // }, [filter, tags])
+
     return (
-      <div className={`${styles.wrapper} ${styles[classFilter]} flex-1`}>
+      <div className={`${styles.wrapper} ${hidden ? styles['hide'] : styles['show']} flex-1`}>
           <Link href={`/${href}`}>
               <a className={`${className} flex transition duration-300 relative box-border text-xs text-white/60 hover:text-white/90  h-44 border hover:border-white/60 border-white/30 min-w-[200px] p-5`}>
                   <Image 
@@ -23,6 +46,7 @@ function Card({tags, title, href, image, className, filter}) {
                   >
                       <Heading className={'font-bold text-lg'} as={'h2'}>{title}</Heading>
                       <p className="m-0">{tags}</p>
+                      <p>{hidden ? 'oculto' : 'visivel'} {filter}</p>
   
                   </div>
               </a>
