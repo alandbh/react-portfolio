3b21518431f6a779a0c4df3d2a785a4e7d601db3
Alan Vasconcelos <alandbh@gmail.com>
2022-08-06 01:53:36 -0300

    Initial work content

--- components/Content/PageData.js
+++ components/Content/PageData.js
@@ -1,7 +1,15 @@
 import { google_cloud } from "./google-cloud";
 import { pwc } from "./pwc";
+import { fundacao_bradesco } from "./fundacao-bradesco";
+import { egov } from "./egov";
+import { rga } from "./rga";
+import { iris } from "./iris";
 
 export const PageData = {
     "google-cloud": google_cloud,
-    pwc: pwc,
+    pwc,
+    "fundacao-bradesco": fundacao_bradesco,
+    egov,
+    rga,
+    iris,
 };
---
+++ components/Content/iris.js
@@ -0,0 +1,60 @@
+export const iris = {
+    title: "IRIS's Website",
+    year: "2020",
+    client: "Institute for Research on Internet & Society (IRIS)",
+    role: "UX, UI, Front-end",
+    project: [
+        `<p>The Goal is to build awareness and demand for PwC in the ESG space, and ultimately help our clients navigate their ESG journey by creating an experience that gives people the confidence in PwC for ESG.</p>
+
+        <p>What is an innovation sprint?</p>
+        
+        <p>It's a four-phase process with a lean, cross-disciplinary team that uses design thinking to create ideas at speed.</p>`,
+    ],
+    access: "https://bit.ly/3cVWmGI",
+    featured_image: "port-featured-pwc.jpg",
+    sections: [
+        {
+            title: "My role",
+            text: [
+                `Heuristic UX Evaluation for E-commerce. We defined 38 heuristics for e-commerce based on a full customer journey, divided into 8 categories.`,
+                `Each category is based on a stage of a purchase journey, starting from the search and identification of the product to the need for support.`,
+            ],
+            image: "port-sec1-google-cloud.svg",
+            color_class: "bg-gray-200",
+        },
+        {
+            title: "Actors",
+            text: [
+                `Two different user profiles who will try to purchase the product on the marketplace websites.`,
+            ],
+            image: "port-sec2-google-cloud.svg",
+        },
+        {
+            title: "Use Cases",
+            text: [
+                `For each actor, I created a set of tasks in order to walk us through the audit. This way we can make a sharper evaluation without lost any scenario.`,
+            ],
+            image: "port-sec3-google-cloud.png",
+            color_class: "bg-white",
+        },
+        {
+            title: "The Heuristics",
+            text: [
+                `Each category is based on a stage of a purchase journey, starting from the search and identification of the product to the need for support.`,
+            ],
+            image: "port-sec4-google-cloud.png",
+            color_class: "bg-gray-100",
+        },
+        {
+            title: "The result",
+            text: [
+                `As a result of the study, Google Cloud Brasil organized a big event at which all the retailer's IT directors were invited. At this event, we presented all the results, and all the improvement opportunities as well.`,
+                `After that, an article was published in Think With Google portal.`,
+                `Click in the link below to see the article.`,
+                `<a class="underline hover:text-gray-800" href="https://bit.ly/3cVWmGI">Article in Think With Google</a>`,
+            ],
+            image: "port-sec5-google-cloud.png",
+            color_class: "bg-white",
+        },
+    ],
+};
