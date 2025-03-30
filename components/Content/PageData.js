a341ba64dabeea5534853196f2fcf03b3f6ca64a
Alan Vasconcelos <alandbh@gmail.com>
2025-03-29 23:19:42 -0300

    Adding Boticario content

--- components/Content/PageData.js
+++ components/Content/PageData.js
@@ -1,3 +1,4 @@
+import { boticario } from "./boticario";
 import { google_cloud } from "./google-cloud";
 import { pwc } from "./pwc";
 import { fundacao_bradesco } from "./fundacao-bradesco";
@@ -6,6 +7,7 @@ import { rga } from "./rga";
 import { iris } from "./iris";
 
 export const PageData = {
+    boticario,
     "google-cloud": google_cloud,
     pwc,
     "fundacao-bradesco": fundacao_bradesco,
---
+++ components/Content/boticario.js
@@ -0,0 +1,88 @@
+export const boticario = {
+    title: "Beauty.B ",
+    year: "2023",
+    client: "Grupo Boticário",
+    role: "UX Research, Design Sprint, Prototyping",
+    company_name: "R/GA",
+    company_url: "https://www.rga.com",
+    project: [
+        `<p>beauty.b is an independent mobile app created for beauty product resellers, designed to make content creation and sharing more intuitive and easy to understand.</p>
+        <p>The app was showcased to Grupo Boticário, one of the world’s largest cosmetics companies.</p>
+        `,
+    ],
+    access: "not available",
+    featured_image: "port-featured-boticario.png",
+    sections: [
+        {
+            title: "The Challenge",
+            text: [
+                `Exploring direct messaging apps for sales represents a huge potential impact for Grupo Boticário.`,
+                `To tackle the challenges, we have listed premises that work to extend Grupo Boticário's excellence to Social Commerce.`,
+            ],
+            text_color_class: "text-white/80",
+            image: "port-sec3-boticario.png",
+            image_classname: "w-[240px]",
+            bg_color_class: "bg-black",
+            image_full: {
+                md: "port-sec3f-boticario.svg",
+                sm: "port-sec2a-boticario-m.png",
+            },
+        },
+        {
+            title: "My role",
+            text: [
+                `I was responsible for the entire UX process, from research to prototyping, and also contributed to parts of the UI design. The research phase included extensive interviews and observations with beauty product resellers, leading to a focused and streamlined design sprint.`,
+                `Since we had a very tight deadline to present the project, by the third day of the Design Sprint we had already completed all the wireframes, allowing the Visual Design team to start working on the branding.`,
+            ],
+            image: "port-sec1-boticario.svg",
+            image_classname: "w-64 md:mt-36 mt-8",
+            bg_color_class: "bg-[#9df1fe] text-black",
+        },
+        {
+            title: "From research to prototyping",
+            text: [
+                `
+                <p>I led both the UX research and the design sprint for this project. We began with in-depth interviews and field research with beauty product resellers to uncover their needs, pain points, and behaviors.</p>`,
+                `<p>Based on these insights, we developed key personas to guide our design decisions. I then facilitated a lean version of the Design Sprint to define priorities and explore solutions within a tight timeframe. <b>By the third day, we had completed all wireframes</b>, allowing the Visual Design team to begin branding.</p>`,
+                `<p>I also led the prototyping phase, from low-fidelity sketches to wireframing, user flows, and finally high-fidelity UI design.</p>`,
+            ],
+            text_color_class: "text-black",
+            image: "port-sec2-boticario.png",
+            image_classname: "w-[300px]",
+            image_full: {
+                md: "port-sec2a-boticario.png",
+                sm: "port-sec2a-boticario-m.png",
+            },
+            bg_color_class: "bg-[#66E897]",
+        },
+
+        {
+            title: "Profile selection",
+            text: [
+                `To accommodate the various accounts of Grupo Boticário brands, resellers can select the brand for which they want to create content, and then select themes, products and exclusive content to create their videos.`,
+            ],
+            text_color_class: "text-black",
+            image: "port-sec4-boticario.gif",
+            bg_color_class: "bg-[#9df1fe]",
+            image_classname: "w-[200px]",
+            image_radius: "rounded-xl",
+        },
+        {
+            title: "Content Creation",
+            text: [
+                `The App's Content Creation allows you to edit key points so that the videos reflect each reseller's personality, while being simple to create and share, in a linear flow and with few decisions.`,
+            ],
+            text_color_class: "text-black",
+            image: "port-sec5-boticario.mp4",
+            bg_color_class: "bg-[#66E897]",
+            video_classname: "w-[200px]",
+            image_radius: "rounded-xl",
+        },
+        {
+            text: [],
+            image_full: "port-sec6-boticario.gif",
+            section_classname: "mt-16",
+            no_padding: true,
+        },
+    ],
+};
